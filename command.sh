e1fe2bdcd3ef493c7657cb097652677edcc1187a
DonizeteVida <donizetevida1998@outlook.com>
2025-09-23 03:42:30 +0200

    Add remaining code

--- command.sh
+++ command.sh
@@ -22,6 +22,7 @@ build() {
 measure() {
     BT=`ls bundletool*.jar`
     IN=$1
+    JSON=$2
 
     for REPO in $(ls $IN); do
         echo "Getting data for ${REPO}"
@@ -35,16 +36,44 @@ measure() {
         BUNDLES=($(find $IN/$REPO -name *.aab | grep -v intermediary))
         echo "BUNDLES found: ${#BUNDLES[*]}"
         echo ${BUNDLES[@]}
-
-        echo -------------------------------
         echo
+
+        for INDEX in ${!BUNDLES[@]}; do
+            DIR=`mktemp --directory`
+
+            BUNDLE=${BUNDLES[$INDEX]}
+            APK=${APKS[$INDEX]}
+            echo "Processing bundle: $BUNDLE"
+            echo "Processing apk: $APK"
+
+            java -jar $BT build-apks                \
+                --bundle=$BUNDLE                    \
+                --output="$DIR/output.apks"         \
+                --device-spec=$JSON                 > /dev/null
+
+            java -jar $BT extract-apks              \
+                --apks="$DIR/output.apks"           \
+                --output-dir=$DIR                   \
+                --device-spec=$JSON                 > /dev/null
+
+            echo "-- SUMMARY --"
+                du -ch $APK
+                du -ch $BUNDLE
+            echo "--"
+                du -ch $DIR/base-*
+            echo "-- -- - -- --"
+
+            echo
+        done
+
+        echo ----------------------------------------------
     done
 }
 
 start() {
     populate data/repos.txt external
     build external
-    measure external > log.txt
+    measure external data/medium_phone_36.json > log.txt
 }
 
 FN=$1
